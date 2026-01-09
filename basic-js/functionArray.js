f702ea2e68425a40dc4c1be9203e74b4e420a8a7
Arfun Ali Yafie <alinews120@gmail.com>
2026-01-09 19:31:57 +0700

    update progress

--- basic-js/functionArray.js
+++ basic-js/functionArray.js
@@ -1,28 +1,99 @@
 //Foreach
-const angka = [1, 2, 3, 4, 5];
+// const angka = [1, 2, 3, 4, 5];
 
-angka.forEach(function (element) {
-    if (element % 5 === 0) {
-        console.log(element);
-    }
-});
+// angka.forEach(function (element) {
+//     if (element % 5 === 0) {
+//         console.log(element);
+//     }
+// });
 
 // ===================contoh lain====================
 
 const animes = [{
     judul: 'Naruto',
-    episode: 220
+    episode: 220,
+    rating: 8.5
 }, {
     judul: 'One Piece',
-    episode: 1000
+    episode: 1000,
+    rating: 9.0
 }, {
     judul: 'Bleach',
-    episode: 366
+    episode: 366,
+    rating: 7.5
+}, {
+    judul: 'Dragon Ball',
+    episode: 153,
+    rating: 8.0
+}, {
+    judul: 'Attack on Titan',
+    episode: 75,
+    rating: 9.5
 }];
 
 animes.forEach(function (anime) {
-    if(anime.episode >= 999){
+    if (anime.episode >= 999) {
         console.log(anime.judul);
     }
 });
+
 //Map
+const angka = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const angkaKaliDua = angka.map(function (num) {
+    return num * 2;
+})
+
+console.log(angkaKaliDua);
+
+const angkaGenap = angka.map(function (num) {
+    if (num % 2 === 0) {
+        return num;
+    }
+})
+
+console.log(angkaGenap);
+
+//Arrow Function
+const pembagian = (a, b) => {
+    if (a === 0 && b === 0) {
+        return "maaf tidak bisa dibagi";
+    }
+    return a / b;
+}
+
+//implisit return
+const perkalian = (a, b) => a * b; //ika function hanya 1 baris dan cenderung proses simpel
+
+const random = (x) => (
+    Math.floor(Math.random() * 50) + 1
+);
+
+//settimeout dan setinterval
+setTimeout(() => {
+    console.log('selamat datang di basic js');
+}, 3000);
+
+const interval = setInterval(() => {
+    console.log('hello world');
+}, 2000);
+
+//untuk menghentikan interval
+clearInterval(interval);
+
+//Filter Method
+//bedanya dengan map, filter mengembalikan nilai array baru berdasarkan kondisi tertentu 
+//sedangkan map mengembalikan nilai array baru berdasarkan proses pada setiap elemennya
+const angkaGanjil = angka.filter(n => {
+    return n * 2;;
+});
+
+const angkaBaru = angka.filter(n => {
+    return n < 5;
+});
+
+const animeRatingBagus = animes.filter(anime => anime.rating >= 8.0);
+const judulAnimeBagus = animeRatingBagus.map((anime) => anime.judul);
+
+//versi gabungan
+// const animeRatingBagus = animes.filter(anime => anime.rating >= 8.0).map(anime => anime.judul);
