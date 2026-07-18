90ac75fa625850967b30782ad91aea9b627b0fae
Arfun Ali Yafie <alinews120@gmail.com>
2026-07-18 14:21:25 +0700

    latihan DOM score game board

--- latihan/papan_skor/skorGame.js
+++ latihan/papan_skor/skorGame.js
@@ -6,33 +6,51 @@ const skorDisplay2 = document.querySelector('#skorDisplay2');
 
 const resetButton = document.querySelector("#Reset");
 const maxSkorDisplay = document.querySelector("#maxSkorDisplay");
-
+const congrats1 = document.querySelector("#congrats1");
+const congrats2 = document.querySelector("#congrats2");
+const winAudio1 = document.querySelector("#audio1");
+const winAudio2 = document.querySelector("#audio2");
+const clickAudio = new Audio("../button_press.mp3");
+const pressAudio = document.querySelector("#pressButton");
+pressAudio.preload = "auto";
+pressAudio.load();
 let skor1 = 0;
 let skor2 = 0;
 winSkor = 3;
 winGame = false;
 
-skorButton1.addEventListener('click', () => {
+skorButton1.addEventListener('click', (e) => {
+    pressAudio.currentTime = 0;
+    pressAudio.play();
     if (!winGame) {
         skor1 += 1;
         if (skor1 == winSkor) {
             winGame = true;
             skorDisplay1.style.backgroundColor = "green";
             skorDisplay1.style.color = "white";
+            congrats1.classList.replace("invisible", "visible");
+            winAudio1.play();
         }
         skorDisplay1.textContent = skor1;
+
     }
 });
 
 skorButton2.addEventListener('click', () => {
+    pressAudio.currentTime = 0;
+    pressAudio.play();
     if (!winGame) {
         skor2 += 1;
+
         if (skor2 == winSkor) {
             winGame = true;
             skorDisplay2.style.backgroundColor = "green";
             skorDisplay2.style.color = "white";
+            congrats2.classList.replace("invisible", "visible");
+            winAudio2.play();
         }
         skorDisplay2.textContent = skor2;
+
     }
 });
 
@@ -44,10 +62,17 @@ function reset() {
     winGame = false;
     skorDisplay1.textContent = skor1;
     skorDisplay2.textContent = skor2;
-    skorDisplay1.style.color = "black";
-    skorDisplay2.style.color = "black";
-    skorDisplay1.style.backgroundColor = "white";
-    skorDisplay2.style.backgroundColor = "white";
+    skorDisplay1.style.color = "red";
+    skorDisplay2.style.color = "red";
+    skorDisplay1.style.backgroundColor = "black";
+    skorDisplay2.style.backgroundColor = "black";
+    congrats1.classList.replace("visible", "invisible");
+    congrats2.classList.replace("visible", "invisible");
+
+    winAudio1.pause();
+    winAudio1.currentTime = 0;
+    winAudio2.pause();
+    winAudio2.currentTime = 0;
 };
 
 maxSkorDisplay.addEventListener('change', (e) => {
